f6c403b84ad50981fef00830d362cf232ba12604
Kiran Busi <kiran.busi@gmail.com>
2012-01-17 21:02:17 +1100

    Django app, create forms for patients to fill out at home

---
+++ phorms/admin.py
@@ -0,0 +1,6 @@
+from phorms.models import Survey, SurveyItem
+from django.contrib import admin
+
+admin.site.register(Survey)
+admin.site.register(SurveyItem)
+
---
+++ phorms/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django import forms
+
+#Forms
+class SurveyForm(forms.Form):
+    question = forms.CharField(max_length=100)
+    answer = forms.CharField(max_length=200)    
+    #cc_myself = forms.BooleanField(required=False)
+
+    
+
+# Models
+class Survey(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class SurveyItem(models.Model):
+    survey = models.ForeignKey(Survey)
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date activated')
+
+
+class Choice(models.Model):
+    survey = models.ForeignKey(SurveyItem)
+    choice = models.CharField(max_length=200)
+    option = models.BooleanField()
+    
