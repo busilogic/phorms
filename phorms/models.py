9aa5796bc1299300b9aa51335744b7b726fa8c2c
Kiran Busi <kiran.busi@gmail.com>
2012-01-23 20:56:20 +1100

    decided to go the admin screen way to add questions dynamically. I could do it myself using jquery but in real time production environment, why reinvent the wheel. Just use django admin and get the job done!
    Registered admin screen with survey and survey title/description

--- phorms/models.py
+++ phorms/models.py
@@ -3,29 +3,26 @@ from django import forms
 
 #Forms
 class SurveyForm(forms.Form):
+    title = forms.CharField(max_length=200)
+    question = forms.CharField(max_length=200)
 
-    question = forms.CharField(max_length=100)
-    answer = forms.CharField(max_length=200)    
-    #cc_myself = forms.BooleanField(required=False)
     
-
-
 
 # Models
 class Survey(models.Model):
     title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    
     def __unicode(self):
-        return self.question
+        return self.title
 
 
 class SurveyItem(models.Model):
     survey = models.ForeignKey(Survey)
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date activated')
-
-
+    
 class Choice(models.Model):
-    survey = models.ForeignKey(SurveyItem)
+    surveyItem = models.ForeignKey(SurveyItem)
     choice = models.CharField(max_length=200)
     option = models.BooleanField()
     
--- phorms/views.py
+++ phorms/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
+from drchrono.phorms.models import Survey, SurveyForm
 
 # Show list of forms created so far no matter who created them
 def list_form(request):
-    c = {}
-    return render_to_response("phorms/list.html", context_instance=RequestContext(request))
-    
-def add_question(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = SurveyForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-        # Process the data in form.cleaned_data
-        # ...
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = SurveyForm() # An unbound form
+    all_surveys = Survey.objects.all()
+    c = {'surveys': all_surveys}
+    return render_to_response("phorms/list.html", c, context_instance=RequestContext(request))
 
-    return render_to_response('contact.html', {'form': form,})
-    
-
-    
-def create_form(request):
-    return render_to_response('phorms/create_survey.html', context_instance=RequestContext(request))
-
+# Handle login
 def login_user(request):
     state = "Please log in ..."
     username = ''
@@ -48,4 +33,16 @@ def login_user(request):
             state = "Incorrect username and/or password!"
 
     return render_to_response('phorms/auth.html', {'state':state, 'username': username},
+                              context_instance=RequestContext(request))
+
+# Create a form
+def create_form(request):
+    if request.method == "POST":
+        #Save the form
+        return render_to_response("phorms/list.html", {}, context_instance=RequestContext(request))
+
+    else:
+        form = SurveyForm()
+        
+    return render_to_response('phorms/create_survey.html', {'form': form},
                               context_instance=RequestContext(request))
