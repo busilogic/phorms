30d3c06eb3601081b7650695a6fc9bb02ea841a9
Kiran Busi <kiran.busi@gmail.com>
2012-01-28 14:49:44 +1100

    Added a wsgi file Added drchrono.phorms so it runs w/o modification on netbook/production server

--- phorms/urls.py
+++ phorms/urls.py
@@ -5,10 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^login/$', 'phorms.views.login_user'),
-    url(r'^create/$', 'phorms.views.create_form'),
-    url(r'^list/$', 'phorms.views.list_form'),
-    url(r'^survey/(?P<survey_id>\d+)/$', 'phorms.views.detail'),                                            
+    url(r'^login/$', 'drchrono.phorms.views.login_user'),
+    url(r'^create/$', 'drchrono.phorms.views.create_form'),
+    url(r'^list/$', 'drchrono.phorms.views.list_form'),
+    url(r'^survey/(?P<survey_id>\d+)/$', 'drchrono.phorms.views.detail'),                                            
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
